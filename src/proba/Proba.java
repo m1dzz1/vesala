0a7fbe69956ff8e8d58fa9bd21aa98dd74ad802e
Stefan Mizdrak <10221stefanmizdrak@etsntesla.edu.rs>
2024-05-25 14:23:36 +0200

    dodao sam loading screen

---
+++ src/proba/Proba.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proba;
+
+import view.loadingScreen;
+
+/**
+ *
+ * @author User
+ */
+public class Proba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
